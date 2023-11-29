36e315e76373f50451747fabf80772e1ba1895be
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-29 18:40:14 +0100

    updated...

--- shopee/cartee/views.py
+++ shopee/cartee/views.py
@@ -3,12 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def store(request){
+def store(request):
     context = {}
     return render(request, 'cartee/store.html', context)
-}
 
-def cart(request){
+
+def cart(request):
     context = {}
     return render(request, 'cartee/cart.html', context)
-}
