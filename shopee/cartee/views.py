63b11c8a824289c8e7dbb036879c2362503710ff
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-30 15:25:19 +0100

    style: Refactor store and cart views
    - Improved consistency in variable assignment and usage.
    - Enhanced readability by avoiding redundant code blocks.
    - Adjusted the 'cart' variable initialization to ensure a consistent structure.
    - Utilized a dictionary for the 'cart' variable in the 'else' block to maintain a consistent structure.
    - Removed unnecessary 'cart' variable assignment in the 'else' block in the 'cart' view

--- shopee/cartee/views.py
+++ shopee/cartee/views.py
@@ -4,8 +4,16 @@ from .models import Product, Cart
 
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user
+        cart, created = Cart.objects.get_or_create(owner = customer, completed = False)
+        cartitems = cart.cartitems_set.all()
+    else:
+        cart = []
+        cartitems = []
+        cart = {'cartquantity' : 0}
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products':products, 'cart':cart, 'cartitems': cartitems}
     return render(request, 'cartee/store.html', context)
 
 
@@ -17,5 +25,6 @@ def cart(request):
     else:
         cart = []
         cartitems = []
+        cart = {'cartquantity' : 0}
     context = {'cart':cart, 'cartitems': cartitems}
     return render(request, 'cartee/cart.html', context)
