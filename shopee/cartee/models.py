3dcf63169ce83d4740df167456c79bf9011ee6a9
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-30 12:42:05 +0100

    completed all fields for the model...

--- shopee/cartee/models.py
+++ shopee/cartee/models.py
@@ -5,8 +5,28 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
+    image = models.ImageField(upload_to='product/image')
     product_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    
+    def __str__(self):
+        return self.name
+
 
 
 class Cart(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    cart_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    completed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.owner.username
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.product
+    
+    
