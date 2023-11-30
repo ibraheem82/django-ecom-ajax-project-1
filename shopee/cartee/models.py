608f69102a62bc5c36e1cf7627ffb094ee6a0e9d
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-30 12:21:49 +0100

    product and cart model was created...

--- shopee/cartee/models.py
+++ shopee/cartee/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+import uuid
+from django.contrib.auth.models import User
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    product_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+
+
+class Cart(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
