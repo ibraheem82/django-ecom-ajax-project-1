07ccd7ae63064a6ba88db84cf2d3b871a83490d0
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-30 19:02:14 +0100

    feat: Enhance 'updateCart' view for better response
    - Retrieved the product name from the product instance for more informative response.
    - Modified the JsonResponse to include a 'message' and 'product_name' for clarity.
    - Ensured that the response is consistent and provides relevant information.

--- shopee/cartee/views.py
+++ shopee/cartee/views.py
@@ -35,6 +35,7 @@ def updateCart(request):
     data = json.loads(request.body)
     product_id = data['product_id']
     action = data['action']
+    # Get the product by its ID
     product = Product.objects.get(product_id = product_id)
     if request.user.is_authenticated:
         customer = request.user
@@ -44,4 +45,8 @@ def updateCart(request):
         if action == 'add':
             cartitems.quantity += 1
         cartitems.save()
-    return JsonResponse('It is working', safe=False)
+    # Get the name of the product
+        product_name = product.name
+        return JsonResponse({'message': 'It is working', 'product_name': product_name}, safe=False)
+    else:
+        return JsonResponse({'message': 'User is not authenticated'}, safe=False)
